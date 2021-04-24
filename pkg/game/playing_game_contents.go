6db24c4123adc634f05785c6906cd002cc7ae87c
Timo Reitz <godsboss@gmx.de>
2021-04-24 18:13:19 +0200

    After defeating the practice targets, show another message.

--- pkg/game/playing_game_contents.go
+++ pkg/game/playing_game_contents.go
@@ -1,9 +1,9 @@
 package game
 
-var playingTriggers map[string]trigger
+var playingTriggers map[string]trigger = make(map[string]trigger)
 
 func init() {
-  playingTriggers = map[string]trigger{
+  pTriggers := map[string]trigger{
     "init": newConditionalTrigger(
       alwaysOK,
       multipleDos(
@@ -23,7 +23,66 @@ func init() {
             },
           },
         ),
+        doAddEnemies(
+          []enemy{
+            {
+              Type: "practice",
+              health: 1000,
+              ramDamage: 1000000, // These guys are *strong*.
+              entity: entity{
+                x: 250,
+                y: 30,
+                w: 24,
+                h: 24,
+              },
+            },
+            {
+              Type: "practice",
+              health: 1000,
+              ramDamage: 1000000,
+              entity: entity{
+                x: 270,
+                y: 80,
+                w: 24,
+                h: 24,
+              },
+            },
+            {
+              Type: "practice",
+              health: 1000,
+              ramDamage: 1000000,
+              entity: entity{
+                x: 220,
+                y: 100,
+                w: 24,
+                h: 24,
+              },
+            },
+          },
+        ),
+        doAddTriggerFromMap("starting_orders", playingTriggers),
+      ),
+    ),
+    "starting_orders": newConditionalTrigger(
+      killedAtLeast("practice", 3),
+      multipleDos(
+        doSetMessage(
+          &message{
+            duration: seconds(30),
+            imageID: "",
+            contents: lines(
+              "Very good!",
+            ),
+          },
+        ),
       ),
     ),
   }
+  for tID := range pTriggers {
+    playingTriggers[tID] = pTriggers[tID]
+  }
+}
+
+func seconds(s int) (ms int) {
+  return s * 1000
 }
