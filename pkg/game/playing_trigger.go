00f297228d75c5540492635a344f580e73b833d0
Timo Reitz <godsboss@gmx.de>
2021-04-24 22:13:19 +0200

    Add check inversion function.

--- pkg/game/playing_trigger.go
+++ pkg/game/playing_trigger.go
@@ -72,6 +72,12 @@ func playerIsDead() func(*playing) bool {
   }
 }
 
+func invertCheck(f func(*playing) bool) func(*playing) bool {
+  return func(p *playing) bool {
+    return !f(p)
+  }
+}
+
 func multipleDos(dos ...func(*playing)) func(*playing) {
   return func(p *playing) {
     for i := range dos {
