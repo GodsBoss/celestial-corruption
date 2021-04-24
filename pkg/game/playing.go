faecc20c1e5e4e0195978f77ec90e9fe63445c46
Timo Reitz <godsboss@gmx.de>
2021-04-24 09:43:05 +0200

    Move player into separate file.

--- pkg/game/playing.go
+++ pkg/game/playing.go
@@ -183,43 +183,6 @@ func entityCollision(e1, e2 entity) (entity, bool) {
   }, true
 }
 
-const (
-  // playerSpeed is the speed of the player in in-game pixels per second.
-  playerSpeed = 100.0
-
-  playerReload = 100
-)
-
-type player struct {
-  entity
-
-  // reload is the time (in ms) weapon needs to reload. Can shoot if zero.
-  reload int
-}
-
-func (p *player) Tick(ms int) {
-  p.reload = max(p.reload - ms, 0)
-}
-
-func (p *player) shoot() []shot {
-  if p.reload > 0 {
-    return nil
-  }
-  p.reload = playerReload
-  _, y := p.Center()
-  return []shot{
-    {
-      entity: entity{
-        x: p.Right(),
-        y: y,
-        w: 4,
-        h: 4,
-      },
-      speedX: 200.0,
-    },
-  }
-}
-
 type shot struct {
   entity
 
---
+++ pkg/game/playing_player.go
@@ -0,0 +1,38 @@
+package game
+
+const (
+  // playerSpeed is the speed of the player in in-game pixels per second.
+  playerSpeed = 100.0
+
+  playerReload = 100
+)
+
+type player struct {
+  entity
+
+  // reload is the time (in ms) weapon needs to reload. Can shoot if zero.
+  reload int
+}
+
+func (p *player) Tick(ms int) {
+  p.reload = max(p.reload - ms, 0)
+}
+
+func (p *player) shoot() []shot {
+  if p.reload > 0 {
+    return nil
+  }
+  p.reload = playerReload
+  _, y := p.Center()
+  return []shot{
+    {
+      entity: entity{
+        x: p.Right(),
+        y: y,
+        w: 4,
+        h: 4,
+      },
+      speedX: 200.0,
+    },
+  }
+}
