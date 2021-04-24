667660f10e902d5698c4f44fc8b5ea9a23388457
Timo Reitz <godsboss@gmx.de>
2021-04-24 04:22:11 +0200

    Remove hint on startup.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GodsBoss/celestial-corruption
 
 go 1.16
+
+require github.com/GodsBoss/gggg v1.1.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+  "github.com/GodsBoss/gggg/pkg/dom"
+)
+
+func main() {
+  win, _ := dom.GlobalWindow()
+  doc, _ := win.Document()
+  hint, _ := doc.GetElementByID("hint")
+  dom.RemoveNode(hint)
+}
